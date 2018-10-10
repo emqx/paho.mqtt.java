dac1a32b013b1dc72308178fde7386aa79c80b56
chongyuan <yinchongyuan@xmeter.net>
2018-10-10 11:08:58 +0800

    Update assigned client id test

--- emq.test.paho/src/test/java/emq/paho/mqtt5/support/test/AssignedClientIdTest.java
+++ emq.test.paho/src/test/java/emq/paho/mqtt5/support/test/AssignedClientIdTest.java
@@ -1,9 +1,12 @@
 package emq.paho.mqtt5.support.test;
 
+import static org.junit.Assert.assertNotNull;
+
 import java.util.logging.Logger;
 
 import org.eclipse.paho.mqttv5.client.MqttAsyncClient;
 import org.eclipse.paho.mqttv5.common.MqttException;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -23,11 +26,22 @@ public class AssignedClientIdTest {
 	
 	@Test
 	public void testAssignedClientId() throws MqttException {
-		String clientId = "";
 		int timeout = 120 * 1000;
 		
+		//null client id
+		String clientId = null;
 		MqttAsyncClient asyncClient = TestClientUtilities.connectAndGetClient(TestHelper.getServerURI().toString(), clientId,
 				null, null, timeout);
+		assertNotNull(asyncClient.getClientId());
+		Assert.assertNotEquals("", asyncClient.getClientId());
+		TestClientUtilities.disconnectAndCloseClient(asyncClient, 5000);
+		
+		//empty client id
+		clientId = "";
+		asyncClient = TestClientUtilities.connectAndGetClient(TestHelper.getServerURI().toString(), clientId,
+				null, null, timeout);
+		assertNotNull(asyncClient.getClientId());
+		Assert.assertNotEquals("", asyncClient.getClientId());
 		TestClientUtilities.disconnectAndCloseClient(asyncClient, 5000);
 	}
 }
